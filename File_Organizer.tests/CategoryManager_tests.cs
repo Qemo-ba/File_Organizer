019dcd8196dfca6a1f15763484a61281d1a44143
Qemo-ba <badalliqemal@gmail.com>
2026-04-18 21:37:54 +0200

    Test upgrade

--- File_Organizer.tests/CategoryManager_tests.cs
+++ File_Organizer.tests/CategoryManager_tests.cs
@@ -8,85 +8,41 @@
              var configData = JsonConfigLoader.LoadConfig("C:\\Users\\qemal\\source\\repos\\File_Organizer\\File_Organizer.tests\\test_files\\test_config.json");
              this.categoryManager = new File_Organizer.CategoryManager(configData);
         }
-        [Fact]
-        public void GetTargetFolder_ImageExtension_ReturnsImagesfolder()
+
+        [Theory]
+        [InlineData(".png", "images")]
+        [InlineData(".pdf", "documents")]
+        [InlineData(".pptx", "presentations")]
+        [InlineData(".xlsx", "spreadsheets")]
+        [InlineData(".mp4", "videos")]
+        public void GetTargetFolder_ValidExtensions_ReturnsCorrectFolder(string extension, string expectedFolder)
         {
-            
-            string ext = ".png";
-            string actual = categoryManager.GetTargetFolder(ext);
-            Assert.Equal("images", actual);
+            string actual = categoryManager.GetTargetFolder(extension);
+            Assert.Equal(expectedFolder, actual);
         }
 
-        [Fact]
-        public void GetTargetFolder_dokumentExtension_Returnsdokumentsfolder()
+        [Theory]
+        [InlineData(".PNG", "images")]
+        [InlineData(".PDF", "documents")]
+        [InlineData(".PPTX", "presentations")]
+        [InlineData(".XLSX", "spreadsheets")]
+        [InlineData(".MP4", "videos")]
+        public void GetTargetFolder_UppercaseExtensions_ReturnsCorrectFolder(string extension, string expectedFolder)
         {
-            string ext = ".pdf";
-            string actual = categoryManager.GetTargetFolder(ext);
-            Assert.Equal("documents", actual);
-        }
-
-        [Fact]
-        public void GetTargetFolder_powerpointExtension_Returnspowerpointsfolder()
-        {
-            string ext = ".pptx";
-            string actual = categoryManager.GetTargetFolder(ext);
-            Assert.Equal("presentations", actual);
-        }
-
-        [Fact]
-        public void GetTargetFolder_excelExtension_Returnsexcelfolder()
-        {
-            string ext = ".xlsx";
-            string actual = categoryManager.GetTargetFolder(ext);
-            Assert.Equal("spreadsheets", actual);
-        }
-
-        [Fact]
-        public void GetTargetFolder_videoExtension_Returnsvideosfolder()
-        {
-            string ext = ".mp4";
-            string actual = categoryManager.GetTargetFolder(ext);
-            Assert.Equal("videos", actual);
+            string actual = categoryManager.GetTargetFolder(extension);
+            Assert.Equal(expectedFolder, actual);
         }
 
 
-        [Fact]
-        public void GetTargetFolder_otherExtension_Returnsotherfolder()
-        {
-            string ext = ".doesnotexist";
-            string actual = categoryManager.GetTargetFolder(ext);
-            Assert.Equal("other", actual);
-        }
 
-        [Fact]
-        public void GetTargetFolder_ToUpperdokumentExtension_returnsdocumentsfolder()
-        {
-            string ext = ".PDF";
-            string actual = categoryManager.GetTargetFolder(ext);
-            Assert.Equal("documents", actual);
-        }
-        
-        [Fact]
-        public void GetTargetFolder_onlyDotExtension()
-        {
-            string ext = ".";
-            string actual = categoryManager.GetTargetFolder(ext);
-            Assert.Equal("other", actual);
-        }
-
-        [Fact]
-        public void GetTargetFolder_ExtensionNULL_returnsothersfolder()
-        {
-            string ext = null;
-            string actual = categoryManager.GetTargetFolder(ext);
-            Assert.Equal("other", actual);
-        }
-
-        [Fact]
-        public void GetTargetFolder_noExtension()
-        {
-            string ext = "";
-            string actual = categoryManager.GetTargetFolder(ext);
+        [Theory]
+        [InlineData(".doesnotexist")]
+        [InlineData(".")]
+        [InlineData(null)]
+        [InlineData("")]
+        public void GetTargetFolder_InvalidExtensions_ReturnsFallbackFolder(string extension) 
+        { 
+            string actual = categoryManager.GetTargetFolder(extension);
             Assert.Equal("other", actual);
         }
 
---
+++ testfolderscript.sh
@@ -0,0 +1,7 @@
+cd C:\Users\qemal\TestFolder
+touch testdoku.pdf
+touch testbild.png
+touch testtabelle.xlsx
+touch testpowerpoint.pptx
+touch testvideo.mp4
+touch testother.py
